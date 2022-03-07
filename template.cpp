cb9e95c756510cc4651a1443595cc9c3ea18b473
AnthonyN1 <anthonyjnool@gmail.com>
2022-03-06 22:06:39 -0500

    [Manager] Include more libraries in template file, with some minor edits

--- template.cpp
+++ template.cpp
@@ -3,19 +3,24 @@
 	Author: Anthony Nool (AnthonyN1)
 */
 
+#include <cassert>
+#include <iostream>
 #include <string>
 #include <vector>
 
 
+/*
+	Helper function.
+*/
 void helper(){
 	return;
 }
 
 
 void partOne(const std::vector<std::string> &input){
-	return;
+	std::cout << "Part one answer" << std::endl;
 }
 
 void partTwo(const std::vector<std::string> &input){
-	return;
+	std::cout << "Part two answer" << std::endl;
 }
