b7f6fb7a2b7185a9147d452f9ce5b73d07a8cf67
AnthonyN1 <anthonyjnool@gmail.com>
2022-03-06 20:35:18 -0500

    [Manager] Add main file, and a runner script
    Users can now run the program using run.sh, passing as command line arguments the year, day, and part of the challenge.

---
+++ main.cpp
@@ -0,0 +1,52 @@
+/*
+	Advent of Code - Main solver
+	Author: Anthony Nool (AnthonyN1)
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+void partOne(const std::vector<std::string> &input);
+void partTwo(const std::vector<std::string> &input);
+
+
+int main(int argc, char* argv[]){
+	// If the number of command line arguments is wrong, returns.
+	if(argc != 3){
+		std::cerr << "ERROR: incorrect number of arguments\n";
+		std::cerr << "USAGE: ./main.exe <input file> <part number>" << std::endl;
+		return 1;
+	}
+
+	// Reads the test in from a text file.
+	std::ifstream inStr(argv[1]);
+	if(!inStr){
+		std::cerr << "ERROR: unable to open input file" << std::endl;
+		return 1;
+	}
+
+	std::vector<std::string> input;
+	std::string line;
+	while(inStr >> line)
+		input.push_back(line);
+	
+	// Determines which part to execute.
+	std::string part(argv[2]);
+	if(part != "1" && part != "2"){
+		std::cerr << "ERROR: <part number> must be either 1 or 2" << std::endl;
+		return 1;
+	}
+
+	int partNum = std::stoi(part);
+	
+	// Runs the function for either part one or part two.
+	switch(partNum){
+		case 1: partOne(input); break;
+		case 2: partTwo(input); break;
+	}
+
+	return 0;
+}
---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Runs a C++ executable to compute the answer for an Advent of Code challenge.
+# Expects three command line arguments:
+# (1) The event year of the requested challenge. Must be a number between 2015 and 2021, with no padding.
+# (2) The day number of the requested challenge. Must be a number between 1 and 25, with no padding.
+# (3) The part number of the event day. Must be either 1 or 2.
+
+
+# If the number of command line arguments is wrong, exits.
+if [[ $# -ne 3 ]]; then
+	echo "ERROR: incorrect number of arguments"
+	echo "USAGE: ./run.sh <year> <day> <part>"
+	exit
+fi
+
+# If the year is invalid, exits.
+re="^20(1[5-9]|2[0-1])$"
+if [[ ! $1 =~ $re ]]; then
+	echo "ERROR: <year> must be an integer between 2015 and 2021, inclusive"
+	exit
+fi
+
+# If the day is invalid, exits.
+re="^([1-9]|1[0-9]|2[0-5])$"
+if [[ ! $2 =~ $re ]]; then
+	echo "ERROR: <day> must be an integer between 1 and 25, inclusive"
+	exit
+fi
+
+# Specifies the path of C++ files and test file of the day.
+padded_day=$(printf "%02d" $2)
+
+day_files="src/$1/day${padded_day}/*.cpp"
+test_file="inputs/$1/day${padded_day}.txt"
+
+# Compiles and runs the program.
+g++ -g $day_files main.cpp -o main.exe -Wall -Wextra && ./main.exe $test_file $3 && rm main.exe
