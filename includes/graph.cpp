4d4040e94c50ef45f6d5ccf8c7328fbf739b695a
AnthonyN1 <anthonyjnool@gmail.com>
2022-03-09 14:03:48 -0500

    [Library] Add Graph class
    The Graph class is a directed weighted graph, with vertices with string names, and with default edge weights of 1.

---
+++ includes/graph.cpp
@@ -0,0 +1,44 @@
+/*
+	Advent of Code - Graph Implementation
+	Author: Anthony Nool (AnthonyN1)
+*/
+
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "graph.h"
+
+//==================================================//
+//                     Observer                     //
+//==================================================//
+/*
+	Returns a vector of all the vertices in the graph.
+*/
+std::vector<std::string> Graph::getVertices() const {
+	std::vector<std::string> verts;
+
+	std::map<std::string, std::set<std::string>>::const_iterator itr;
+	for(itr = adjList_.cbegin(); itr != adjList_.cend(); ++itr)
+		verts.push_back(itr->first);
+	
+	return verts;
+}
+
+
+//==================================================//
+//                     Mutator                      //
+//==================================================//
+/*
+	Adds an edge between v1 and v2 with weight weight to the graph.
+*/
+void Graph::addEdge(const std::string &v1, const std::string &v2, unsigned long long weight){
+	adjList_[v1].insert(v2);
+	weights_[std::make_pair(v1, v2)] = weight;
+
+	// If v2 doesn't has a current out-degree of 0, adds it to the adjacency list.
+	if(adjList_.find(v2) == adjList_.end())
+		adjList_[v2] = std::set<std::string>();
+}
---
+++ includes/graph.h
@@ -0,0 +1,29 @@
+/*
+	Advent of Code - Graph Header
+	Author: Anthony Nool (AnthonyN1)
+*/
+
+#ifndef __graph_h_
+#define __graph_h_
+
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+
+
+class Graph{
+	public:
+		// Observers
+		std::vector<std::string> getVertices() const;
+		unsigned long long getWeight(const std::string &v1, const std::string &v2) const { return weights_.at(std::make_pair(v1, v2)); }
+		bool hasEdge(const std::string &v1, const std::string &v2) const { return adjList_.at(v1).find(v2) != adjList_.at(v1).end(); }
+
+		// Mutator
+		void addEdge(const std::string &v1, const std::string &v2, unsigned long long weight);
+	private:
+		std::map<std::string, std::set<std::string>> adjList_;
+		std::map<std::pair<std::string, std::string>, unsigned long long> weights_;
+};
+
+#endif
