a7a74583acb531c438f61a7066798e9b3c403463
AnthonyN1 <anthonyjnool@gmail.com>
2022-11-30 16:25:22 -0500

    [Library] Add Javadoc comments to Utils, and remove Stars and Bars algorithm

--- includes/utils.cpp
+++ includes/utils.cpp
@@ -10,19 +10,26 @@
 #include "utils.h"
 
 
-/*
-	Returns true iff str is a number.
+/**
+ * @param str  the string to parse
+ * 
+ * @return true if and only if the string can be parsed into a non-negative number.
+ * 
 */
 bool Utils::isNumber(const std::string &str){
 	for(char ch : str)
 		if(!isdigit(ch))
-			return false;
-	
+			return false;	
 	return true;
 }
 
-/*
-	Splits str with a delimiter of delim.
+
+/**
+ * @param str    the string to split
+ * @param delim  the delimiting character
+ * 
+ * @return a collection of substrings created by splitting the string by the delimiting string
+ * 
 */
 std::vector<std::string> Utils::split(const std::string &str, const std::string &delim){
 	std::vector<std::string> parts;
@@ -35,50 +42,4 @@ std::vector<std::string> Utils::split(const std::string &str, const std::string 
 	parts.push_back(str.substr(start));
 
 	return parts;
-}
-
-/*
-	Returns all the distinct partitions of n with k parts. That is, the different ways n can be split into k addends (including 0).
-	Uses the method of Stars and Bars from combinatorics.
-*/
-namespace{
-	void starsAndBarsRecurse(unsigned long long numStarsLeft, unsigned long long numTotalBins, std::vector<unsigned long long> &currBin, std::vector<std::vector<unsigned long long>> &allBins){
-		// Base case: no stars left.
-		// Adds 0s to the rest of the bins, and adds the partition to allBins.
-		if(numStarsLeft == 0){
-			unsigned int prevSize = currBin.size();
-
-			while(currBin.size() != numTotalBins) currBin.push_back(0);
-			allBins.push_back(currBin);
-			while(currBin.size() != prevSize) currBin.pop_back();
-
-			return;
-		}
-		
-		// Base case: one bin left to fill.
-		// Fills the last bin with the remaining stars, and adds the partition to allBins.
-		if(numTotalBins - currBin.size() == 1){
-			currBin.push_back(numStarsLeft);
-			allBins.push_back(currBin);
-			currBin.pop_back();
-
-			return;
-		}
-
-		// General case.
-		// Iteratively fills the next bin with an appropriate number of stars, and recurrences to the next bins.
-		for(unsigned long long numStarsNext = 0; numStarsNext <= numStarsLeft; ++numStarsNext){
-			currBin.push_back(numStarsNext);
-			starsAndBarsRecurse(numStarsLeft - numStarsNext, numTotalBins, currBin, allBins);
-			currBin.pop_back();
-		}
-	}
-}
-
-std::vector<std::vector<unsigned long long>> Utils::starsAndBars(unsigned long long n, unsigned long long k){
-	std::vector<unsigned long long> currBin;
-	std::vector<std::vector<unsigned long long>> allBins;
-	starsAndBarsRecurse(n, k, currBin, allBins);
-
-	return allBins;
 }
--- includes/utils.h
+++ includes/utils.h
@@ -13,7 +13,6 @@
 namespace Utils{
 	bool isNumber(const std::string &str);
 	std::vector<std::string> split(const std::string &str, const std::string &delim);
-	std::vector<std::vector<unsigned long long>> starsAndBars(unsigned long long n, unsigned long long k);
 }
 
 #endif
