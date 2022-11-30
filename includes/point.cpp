0ffcf235598faee88c623a131ce414c8e7b71a26
AnthonyN1 <anthonyjnool@gmail.com>
2022-11-30 13:42:37 -0500

    [Library] Add Javadoc comments to Point

--- includes/point.cpp
+++ includes/point.cpp
@@ -3,18 +3,28 @@
 	Author: Anthony Nool (AnthonyN1)
 */
 
-#include <string>
-
 #include "point.h"
 
 
 //==================================================//
-//                     Mutator                      //
+//                     Modifiers                    //
 //==================================================//
-/*
-	Translates the point with a specified change in x and y.
+/**
+ * @brief Translates the current point by the specified deltas.
+ * 
+ * @param deltaX  the change in the x-coordinate
+ * @param deltaY  the change in the y-coordinate
+ * 
 */
 void Point::shift(long long deltaX, long long deltaY){
 	x_ += deltaX;
 	y_ += deltaY;
+}
+
+
+//==================================================//
+//                     Operators                    //
+//==================================================//
+bool operator<(const Point &p1, const Point &p2){
+	return (p1.getX() != p2.getX()) ? p1.getX() < p2.getX() : p1.getY() < p2.getY();
 }
--- includes/point.h
+++ includes/point.h
@@ -6,25 +6,23 @@
 #ifndef __point_h_
 #define __point_h_
 
-#include <string>
-
 
 class Point{
 	public:
-		// Constructor
+		// Constructors
 		Point(long long x=0, long long y=0): x_(x), y_(y) {}
 
-		// Observers
+		// Accessors
 		long long getX() const { return x_; }
 		long long getY() const { return y_; }
 
-		// Mutator
+		// Modifiers
 		void shift(long long deltaX, long long deltaY);
-
-		// Operator
-		bool operator<(const Point &other) const { return (x_ != other.x_) ? x_ < other.x_ : y_ < other.y_; }
 	private:
 		long long x_, y_;
 };
+
+// Operators
+bool operator<(const Point &p1, const Point &p2);
 
 #endif
