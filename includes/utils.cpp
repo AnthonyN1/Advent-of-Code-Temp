e44644c286b8a44b56be10c1801f209e840eb2f0
AnthonyN1 <anthonyjnool@gmail.com>
2022-12-01 17:05:56 -0500

    [Library] Add join() to Utils

--- includes/utils.cpp
+++ includes/utils.cpp
@@ -25,6 +25,24 @@ bool Utils::isNumber(const std::string &str){
 
 
 /**
+ * @param strs   the collection of strings to join
+ * @param delim  the delimiting character
+ * 
+ * @return a string comprising of the given strings separated by the delimiting string.
+ * 
+*/
+std::string Utils::join(const std::vector<std::string> &strs, const std::string &delim){
+	std::string str;
+	for(unsigned int i = 0; i < strs.size(); ++i){
+		str += strs[i];
+		if(i != strs.size() - 1) str += delim;
+	}
+
+	return str;
+}
+
+
+/**
  * @param str    the string to split
  * @param delim  the delimiting character
  * 
--- includes/utils.h
+++ includes/utils.h
@@ -16,6 +16,7 @@
 */
 namespace Utils{
 	bool isNumber(const std::string &str);
+	std::string join(const std::vector<std::string> &strs, const std::string &delim);
 	std::vector<std::string> split(const std::string &str, const std::string &delim);
 }
 
