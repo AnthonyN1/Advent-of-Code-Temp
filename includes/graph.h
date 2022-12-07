1d87ad105080bdca3564e187cd5ead4435e8de50
AnthonyN1 <anthonyjnool@gmail.com>
2022-12-07 04:41:44 -0500

    [Library] Update Javadoc comments in Graph

--- includes/graph.h
+++ includes/graph.h
@@ -12,24 +12,64 @@
 
 
 /**
- * @brief A directed Graph with optional weighted edges.
+ * @brief A directed graph with optional weighted edges.
  * 
  * @tparam V  the type of vertex labels
  * 
 */
 template <class V> class Graph{
 	public:
-		// Accessors
+		//==================================================//
+		//                     Accessors                    //
+		//==================================================//
+		/**
+		 * @return a set of the vertex labels in the graph.
+		 * 
+		*/
 		std::set<V> getVertices() const;
+
+		/**
+		 * @param v1  the label of the source vertex
+		 * @param v2  the label of the destination vertex
+		 * 
+		 * @return the weight of the edge between the vertices labeled by v1 and v2.
+		 * 
+		*/
 		long long getWeight(const V &v1, const V &v2) const { return weights_.at(std::make_pair(v1, v2)); }
+
+		/**
+		 * @param v1  the label of the source vertex
+		 * @param v2  the label of the destination vertex
+		 * 
+		 * @return true if the graph has an edge from the vertices labeled by v1 and v2, and false otherwise.
+		 * 
+		*/
 		bool hasEdge(const V &v1, const V &v2) const { return adjList_.at(v1).find(v2) != adjList_.at(v1).end(); }
 
-		// Modifiers
-		void addEdge(const V &v1, const V &v2, long long weight=1);
+
+		//==================================================//
+		//                     Modifiers                    //
+		//==================================================//
+		/**
+		 * @brief Adds an edge to the graph between two vertices with an optional weight.
+		 * 
+		 * @param v1  the label of the source vertex
+		 * @param v2  the label of the destination vertex
+		 * @param w   the edge weight
+		 * 
+		*/
+		void addEdge(const V &v1, const V &v2, long long w=1);
+
+		/**
+		 * @brief Adds a vertex to the graph.
+		 * 
+		 * @param v  the label of the vertex
+		 * 
+		*/
 		void addVertex(const V &v);
 	private:
-		std::map<V, std::set<V>> adjList_;
-		std::map<std::pair<V, V>, long long> weights_;
+		std::map<V, std::set<V>> adjList_;					/** An adjacency list representation of a mathematical graph. */
+		std::map<std::pair<V, V>, long long> weights_;		/** A map of edges to their weights. */
 };
 
 #include "graph.hpp"
--- includes/graph.hpp
+++ includes/graph.hpp
@@ -8,13 +8,6 @@
 #include <utility>
 
 
-//==================================================//
-//                     Accessors                    //
-//==================================================//
-/**
- * @return a collection of the vertex labels in the graph.
- * 
-*/
 template <class V> std::set<V> Graph<V>::getVertices() const {
 	std::set<V> verts;
 
@@ -26,28 +19,11 @@ template <class V> std::set<V> Graph<V>::getVertices() const {
 }
 
 
-//==================================================//
-//                     Modifiers                    //
-//==================================================//
-/**
- * @brief Adds an edge from v1 to v2 with a specified weight to the graph.
- * 
- * @param v1      the label of the source vertex
- * @param v2      the label of the destination vertex
- * @param weight  the edge weight
- * 
-*/
-template <class V> void Graph<V>::addEdge(const V &v1, const V &v2, long long weight){
+template <class V> void Graph<V>::addEdge(const V &v1, const V &v2, long long w){
 	adjList_[v1].insert(v2);
-	weights_[std::make_pair(v1, v2)] = weight;
+	weights_[std::make_pair(v1, v2)] = w;
 }
 
-/**
- * @brief Adds a vertex to the graph.
- * 
- * @param v  the label of the vertex
- * 
-*/
 template <class V> void Graph<V>::addVertex(const V &v){
 	if(adjList_.find(v) == adjList_.end())
 		adjList_[v] = std::set<V>();
