633f8acda9549d5c3a76ac529662be07abba474d
AnthonyN1 <anthonyjnool@gmail.com>
2022-12-07 05:39:50 -0500

    [Library] Update Javadoc comments in Point

--- includes/point.cpp
+++ includes/point.cpp
@@ -6,25 +6,12 @@
 #include "point.h"
 
 
-//==================================================//
-//                     Modifiers                    //
-//==================================================//
-/**
- * @brief Translates the current point by the specified deltas.
- * 
- * @param deltaX  the change in the x-coordinate
- * @param deltaY  the change in the y-coordinate
- * 
-*/
 void Point::shift(long long deltaX, long long deltaY){
 	x_ += deltaX;
 	y_ += deltaY;
 }
 
 
-//==================================================//
-//                     Operators                    //
-//==================================================//
 bool operator<(const Point &p1, const Point &p2){
 	return (p1.getX() != p2.getX()) ? p1.getX() < p2.getX() : p1.getY() < p2.getY();
 }
--- includes/point.h
+++ includes/point.h
@@ -13,20 +13,59 @@
 */
 class Point{
 	public:
-		// Constructors
+		//==================================================//
+		//                   Constructors                   //
+		//==================================================//
+		/**
+		 * @brief Constructs a Point with a coordinate of (x, y).
+		 * 
+		 * @param x  the x-coordinate of the point
+		 * @param y  the y-coordinate of the point
+		 * 
+		*/
 		Point(long long x=0, long long y=0): x_(x), y_(y) {}
 
-		// Accessors
+		//==================================================//
+		//                     Accessors                    //
+		//==================================================//
+		/**
+		 * @return the x-coordinate of the point.
+		 * 
+		*/
 		long long getX() const { return x_; }
+
+		/**
+		 * @return the y-coordinate of the point.
+		 * 
+		*/
 		long long getY() const { return y_; }
 
-		// Modifiers
+		//==================================================//
+		//                     Modifiers                    //
+		//==================================================//
+		/**
+		 * @brief Translates the point by the deltas.
+		 * 
+		 * @param deltaX  the change in the x-coordinate
+		 * @param deltaY  the change in the y-coordinate
+		 * 
+		*/
 		void shift(long long deltaX, long long deltaY);
 	private:
-		long long x_, y_;
+		long long x_;		/** The x-coordinate. */
+		long long y_;		/** The y-coordinate. */
 };
 
-// Operators
+//==================================================//
+//                     Operators                    //
+//==================================================//
+/**
+ * @param p1  the first Point
+ * @param p2  the second Point
+ * 
+ * @return true if p1 is before p2 in dictionary order, and false otherwise.
+ * 
+*/
 bool operator<(const Point &p1, const Point &p2);
 
 #endif
