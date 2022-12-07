12cb44d3e5513c785ec950912349b119dad71298
AnthonyN1 <anthonyjnool@gmail.com>
2022-12-07 05:24:57 -0500

    [Library] Update Javadoc comments in Grid

--- includes/grid.h
+++ includes/grid.h
@@ -15,27 +15,111 @@
 */
 template <class T> class Grid{
 	public:
-		// Constructors
+		//==================================================//
+		//                   Constructors                   //
+		//==================================================//
+		/**
+		 * @brief Constructs a Grid with the r rows and c columns, filling the cells with an optional value init.
+		 * 
+		 * @param r     the number of rows in the grid
+		 * @param c     the number of columns in the grid
+		 * @param init  the initial value to fill the cells with
+		 * 
+		*/
 		Grid(unsigned int rows=0, unsigned int cols=0, const T &init=T());
+
+		/**
+		 * @brief Constructs a Grid using g.
+		 * 
+		 * @param g  the grid to copy from
+		 * 
+		*/
 		Grid(const Grid<T> &g){ copy(g); }
+		
+		/**
+		 * @brief Assigns the current grid to g.
+		 * 
+		 * @param g  the grid to copy from
+		 * 
+		 * @return a reference to the current grid.
+		 * 
+		*/
 		Grid<T> & operator=(const Grid<T> &g);
+
+		/**
+		 * @brief Destroys the grid.
+		 * 
+		*/
 		~Grid(){ destroy(); }
 
-		// Accessors
+		//==================================================//
+		//                     Accessors                    //
+		//==================================================//
+		/**
+		 * @param r  the index of the row of the grid cell
+		 * @param c  the index of the column of the grid cell
+		 * 
+		 * @return the value of the grid cell at row r and column c.
+		 * 
+		*/
 		const T & at(unsigned int r, unsigned int c) const { return grid_[r][c]; }
+
+		/**
+		 * @param val  the value to look for
+		 * 
+		 * @return the number of cells whose value matches val.
+		 * 
+		*/
 		unsigned long long countMatching(const T &val) const;
+
+		/**
+		 * @return the number of columns in the grid.
+		 * 
+		*/
 		unsigned int getCols() const { return cols_; }
+
+		/**
+		 * @return the number of rows in the grid.
+		 * 
+		*/
 		unsigned int getRows() const { return rows_; }
+
+		/**
+		 * @return the sum of all values in the grid.
+		 * 
+		*/
 		T sum() const;
 
-		// Modifiers
+		//==================================================//
+		//                     Modifiers                    //
+		//==================================================//
+		/**
+		 * @brief Updates the grid cell at row r and column c to val.
+		 * 
+		 * @param r    the index of the row of the grid cell
+		 * @param c    the index of the column of the grid cell
+		 * @param val  the value to insert into the grid
+		 * 
+		*/
 		void update(unsigned int r, unsigned int c, const T &val){ grid_[r][c] = val; }
 	private:
+		/**
+		 * @brief Constructs a Grid with the specified rows and columns, filling the cells with an optional value.
+		 * 
+		 * @param g  the graph to copy from
+		 * 
+		*/
 		void copy(const Grid<T> &g);
+
+		/**
+		 * @brief Deletes all dynamically allocated memory.
+		 * 
+		*/
 		void destroy();
 
-		unsigned int rows_, cols_;
-		T** grid_;
+		unsigned int rows_;		/** The number of rows in the grid. */
+		unsigned int cols_;		/** The number of columns in the grid. */
+		T** grid_;				/** A pointer representation of the grid. */
 };
 
 #include "grid.hpp"
--- includes/grid.hpp
+++ includes/grid.hpp
@@ -6,20 +6,9 @@
 #include <algorithm>
 
 
-//==================================================//
-//                   Constructors                   //
-//==================================================//
-/**
- * @brief Constructs a Grid with the specified rows and columns, filling the cells with an optional value.
- * 
- * @param rows  the number of rows in the grid
- * @param cols  the number of columns in the grid
- * @param init  the initial value to fill the cells with
- * 
-*/
-template <class T> Grid<T>::Grid(unsigned int rows, unsigned int cols, const T &init){
-	rows_ = rows;
-	cols_ = cols;
+template <class T> Grid<T>::Grid(unsigned int r, unsigned int c, const T &init){
+	rows_ = r;
+	cols_ = c;
 
 	grid_ = new T*[rows_];
 	for(unsigned int r = 0; r < rows_; ++r){
@@ -29,14 +18,6 @@ template <class T> Grid<T>::Grid(unsigned int rows, unsigned int cols, const T &
 	}
 }
 
-/**
- * @brief Assigns the current grid to the specified grid.
- * 
- * @param g  the grid to copy from
- * 
- * @return a reference to the current grid.
- * 
-*/
 template <class T> Grid<T> & Grid<T>::operator=(const Grid<T> &g){
 	if(&g != this){
 		destroy();
@@ -47,15 +28,6 @@ template <class T> Grid<T> & Grid<T>::operator=(const Grid<T> &g){
 }
 
 
-//==================================================//
-//                     Accessors                    //
-//==================================================//
-/**
- * @param val  the value to look for
- * 
- * @return the number of cells whose value matches the specified value.
- * 
-*/
 template <class T> unsigned long long Grid<T>::countMatching(const T &val) const {
 	unsigned long long matching = 0;
 	for(unsigned int r = 0; r < rows_; ++r)
@@ -64,10 +36,6 @@ template <class T> unsigned long long Grid<T>::countMatching(const T &val) const
 	return matching;
 }
 
-/**
- * @return the sum of all values in the grid.
- * 
-*/
 template <class T> T Grid<T>::sum() const {
 	T sum = T();
 	for(unsigned int r = 0; r < rows_; ++r)
@@ -78,15 +46,6 @@ template <class T> T Grid<T>::sum() const {
 }
 
 
-//==================================================//
-//                  Private helpers                 //
-//==================================================//
-/**
- * @brief Constructs a Grid with the specified rows and columns, filling the cells with an optional value.
- * 
- * @param g  the graph to copy from
- * 
-*/
 template <class T> void Grid<T>::copy(const Grid<T> &g){
 	rows_ = g.rows_;
 	cols_ = g.cols_;
@@ -99,10 +58,6 @@ template <class T> void Grid<T>::copy(const Grid<T> &g){
 	}
 }
 
-/**
- * @brief Deletes all dynamically allocated memory.
- * 
-*/
 template <class T> void Grid<T>::destroy(){
 	// Deletes all row arrays.
 	for(unsigned int r = 0; r < rows_; ++r)
