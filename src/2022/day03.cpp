f25131848ff69ad813e6d4d46cc88fb878f10694
AnthonyN1 <anthonyjnool@gmail.com>
2022-12-13 11:00:37 -0500

    [Refactor] 2022: Day 03: Abstract input parsing to a helper function

--- src/2022/day03.cpp
+++ src/2022/day03.cpp
@@ -8,22 +8,52 @@
 #include <iterator>
 #include <set>
 #include <string>
+#include <utility>
 #include <vector>
 
 
 /**
- * @brief Separates a string into two equal (or close to equal) substrings.
+ * @brief Parses the puzzle input for part one.
  * 
- * @param input  the string to split
- * @param c1     the first half of the input string
- * @param c2     the second half of the input string
+ * @param input   the puzzle input
+ * @param parsed  the input, parsed into a convenient data structure
  * 
 */
-void separate(const std::string &input, std::string &c1, std::string &c2){
-	unsigned int midpoint = input.size() / 2;
-	c1 = input.substr(0, midpoint);
-	c2 = input.substr(midpoint);
+void parseInputPartOne(const std::vector<std::string> &input, std::vector<std::pair<std::set<char>, std::set<char>>> &parsed){
+	for(const std::string &line : input){
+		unsigned int midpoint = line.size() / 2;
+		std::string str1 = line.substr(0, midpoint);
+		std::string str2 = line.substr(midpoint);
+		
+		std::set<char> c1(str1.begin(), str1.end());
+		std::set<char> c2(str2.begin(), str2.end());
+
+		parsed.push_back(std::make_pair(c1, c2));
+	}
 }
+
+/**
+ * @brief Parses the puzzle input for part two.
+ * 
+ * @param input   the puzzle input
+ * @param parsed  the input, parsed into a convenient data structure
+ * 
+*/
+void parseInputPartTwo(const std::vector<std::string> &input, std::vector<std::vector<std::set<char>>> &parsed){
+	for(unsigned int i = 0; i < input.size() / 3; ++i){
+		std::string str1 = input[3 * i];
+		std::set<char> c1(str1.begin(), str1.end());
+
+		std::string str2 = input[3 * i + 1];
+		std::set<char> c2(str2.begin(), str2.end());
+
+		std::string str3 = input[3 * i + 2];
+		std::set<char> c3(str3.begin(), str3.end());
+
+		parsed.push_back({c1, c2, c3});
+	}
+}
+
 
 /**
  * @param c1  a set of characters
@@ -56,18 +86,15 @@ unsigned int getPriority(char ch){
 
 
 void partOne(const std::vector<std::string> &input){
+	std::vector<std::pair<std::set<char>, std::set<char>>> compartments;
+	parseInputPartOne(input, compartments);
+
+
 	// Sums the priority of each common item type.
 	unsigned long long sum = 0;
-	for(const std::string &line : input){
-		// Separates the line (rucksack) into two compartments.
-		std::string str1, str2;
-		separate(line, str1, str2);
-
-		std::set<char> c1(str1.begin(), str1.end());
-		std::set<char> c2(str2.begin(), str2.end());
-
+	for(const std::pair<std::set<char>, std::set<char>> &rucksack : compartments){
 		// Gets the common item type.
-		std::set<char> commonTypes = getCommonTypes(c1, c2);
+		std::set<char> commonTypes = getCommonTypes(rucksack.first, rucksack.second);
 		char commonType = *commonTypes.begin();
 
 		// Adds the priority to the sum.
@@ -79,22 +106,16 @@ void partOne(const std::vector<std::string> &input){
 }
 
 void partTwo(const std::vector<std::string> &input){
+	std::vector<std::vector<std::set<char>>> groups;
+	parseInputPartTwo(input, groups);
+
+
 	// Sums the priority of each common item type across three rucksacks.
 	unsigned long long sum = 0;
-	for(unsigned int i = 0; i < input.size() / 3; ++i){
-		// Constructs sets of characters representing the rucksacks.
-		std::string str1 = input[3 * i];
-		std::set<char> c1(str1.begin(), str1.end());
-
-		std::string str2 = input[3 * i + 1];
-		std::set<char> c2(str2.begin(), str2.end());
-
-		std::string str3 = input[3 * i + 2];
-		std::set<char> c3(str3.begin(), str3.end());
-
+	for(const std::vector<std::set<char>> &group : groups){
 		// Gets the common item type.
-		std::set<char> temp = getCommonTypes(c1, c2);
-		std::set<char> commonTypes = getCommonTypes(temp, c3);
+		std::set<char> temp = getCommonTypes(group[0], group[1]);
+		std::set<char> commonTypes = getCommonTypes(temp, group[2]);
 		char commonType = *commonTypes.begin();
 
 		// Adds the priority to the sum.
